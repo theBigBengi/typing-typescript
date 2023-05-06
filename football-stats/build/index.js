0358e001261d1459fd5e558b62c920967cd0cb64
theBigBengi <imthebigbengi@gmail.com>
2023-05-06 23:37:33 +0300

    Reading from CSV test

--- football-stats/build/index.js
+++ football-stats/build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("object");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const data = fs_1.default
+    .readFileSync("football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map((match) => match.split(","));
+console.log(data);
--- football-stats/src/index.ts
+++ football-stats/src/index.ts
@@ -1,1 +1,8 @@
-console.log("object");
+import fs from "fs";
+
+const data = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((match: string): string[] => match.split(","));
+
+console.log(data);
