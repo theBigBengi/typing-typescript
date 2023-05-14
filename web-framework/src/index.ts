23e54f325182ac1bb9a879a0547ab9d59488e54f
theBigBengi <imthebigbengi@gmail.com>
2023-05-14 22:10:27 +0300

    Modeling User

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,9 +1,11 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "dor", age: 35 });
+const user = new User({ id: 1, name: "dor", age: 35 });
 
-console.log(user.get("name"));
+user.set({ name: "ana2t" });
 
-user.set({ name: "anat" });
-console.log(user.get("name"));
-console.log(user.get("age"));
+const user2 = new User({ name: "sdadasdr", age: 335 });
+
+user2.save();
+
+user.save();
--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -1,9 +1,16 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  private events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propertyName: string): number | string {
@@ -12,5 +19,35 @@ export class User {
 
   set(properties: UserProps): void {
     Object.assign(this.data, properties);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const callbacks = this.events[eventName] || [];
+    callbacks.push(callback);
+    this.events[eventName] = callbacks;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers) return;
+
+    handlers.forEach((h) => h());
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse) => this.set(response.data))
+      .catch((err) => console.log(err));
+  }
+
+  save(): void {
+    const id = this.get("id");
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data);
+    }
   }
 }
